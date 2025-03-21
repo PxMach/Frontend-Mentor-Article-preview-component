65b41ccd4ab1a8703d11b12c17b5ffb71be2c6d8
PxMach <moundjidmachghour58@gmail.com>
2025-03-22 00:21:31 +0100

    Initialization

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const share = document.querySelector(".share");
+const socialMedia = document.querySelector(".social-media");
+
+share.addEventListener("click", function () {
+   socialMedia.style.display = "flex";
+});
+
