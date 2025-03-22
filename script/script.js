1d11e66716060265f754c1f14d0ca97a055f6134
PxMach <moundjidmachghour58@gmail.com>
2025-03-22 11:57:14 +0100

    Mise à jour des commentaires dans le CSS et le JavaScript pour une meilleure clarté. Changement de termes français en anglais pour uniformiser le code.

--- script/script.js
+++ script/script.js
@@ -2,9 +2,9 @@ const share = document.querySelector(".share");
 const socialMedia = document.querySelector(".social-media");
 
 share.addEventListener("click", function () {
-   socialMedia.classList.toggle("visible"); // Ajoute ou supprime la classe "visible"
+   socialMedia.classList.toggle("visible"); // Adds or deletes the “visible” class"
 });
 
 share.addEventListener("click", function () {
-   share.classList.toggle("active"); // Ajoute ou supprime la classe "active"
+   share.classList.toggle("active"); // Adds or deletes “active” class"
 });
