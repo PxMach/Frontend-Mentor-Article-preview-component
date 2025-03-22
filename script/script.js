b3452066a718353f6696bca53bf2a4c09f503a81
PxMach <moundjidmachghour58@gmail.com>
2025-03-22 11:55:18 +0100

    Ajout de la classe "nav-profile" pour la section de profil et réorganisation du balisage social. Mise à jour du CSS pour améliorer la disposition et l'interaction des éléments, y compris l'animation de partage. Amélioration du script pour gérer l'état actif du bouton de partage.

--- script/script.js
+++ script/script.js
@@ -4,3 +4,7 @@ const socialMedia = document.querySelector(".social-media");
 share.addEventListener("click", function () {
    socialMedia.classList.toggle("visible"); // Ajoute ou supprime la classe "visible"
 });
+
+share.addEventListener("click", function () {
+   share.classList.toggle("active"); // Ajoute ou supprime la classe "active"
+});
